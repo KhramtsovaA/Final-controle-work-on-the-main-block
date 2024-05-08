403006b587f6a4f6cad95ee93ade9d0895962688
KhramtsovaA <Khramtsova.anna2016@gmail.com>
2024-05-08 17:58:07 +0300

    Схема

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write($"Введите значения через запятую в одну строчку для использования в массиве: ");
+
+string? word = Console.ReadLine();
+
+string[] CreateArray()      // Создаем массив из введенных элементов.
+{
+   
+    string[] arrayAll = word.Split(",");
+    return arrayAll;
+}
+
+string[] FinalArray()  // Метод для создания массива cогласно условиям.
+{
+    string[] array = CreateArray();
+    string Finalstring = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Finalstring = Finalstring + array[i] + ",";
+        }
+        else
+        {
+            Finalstring = " ";
+        }
+    }
+    Finalstring = Finalstring.Remove(Finalstring.Length - 1); 
+    string[] NewArray = Finalstring.Split(",");
+    return NewArray;
+}
+
+string[] array = CreateArray();
+System.Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]"); // Исходный массив.
+
+System.Console.WriteLine($"Массив согласно условиям: [{string.Join(", ", FinalArray())}]");
+
+
